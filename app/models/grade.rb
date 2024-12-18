fd70614e5f7e95d17dbb17a9d7ab02089e3b6074
Lucas de Amystherdam <pro.lucas07@gmail.com>
2024-12-17 21:33:17 -0300

    Progressive aggregation of grades in averages

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -5,4 +5,17 @@ class Grade < ApplicationRecord
   belongs_to :subject
 
   validates :obtained, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+
+  after_create :grades_progressive_aggregation
+
+  private
+
+  def grades_progressive_aggregation
+    cicle = Cicle.find_by(month: Time.now.utc.month, year: Time.now.utc.year)
+
+    return if cicle.nil?
+
+    GradeAggregation.new(student:, subject:, cicle:).calculate_final_subject_grades_by_student
+    GradeAggregation.new(student:, cicle:).calculate_student_final_grade
+  end
 end
